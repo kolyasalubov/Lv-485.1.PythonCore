d56df595a8cf0d654aa5dfef6e72f2d9a1137f16
Mykola Nakonechnyi <m.v.m.nak@gmail.com>
2020-02-04 14:40:52 +0200

    add new file

---
+++ HW.1Les.py
@@ -0,0 +1,11 @@
+a = int(input("a="))
+b = int(input("b="))
+print("a+b=", a+b)
+print("a-b=", a-b)
+print("a*b=", a*b)
+print("a/b=", a/b)
+
+name = input("What is your name?")
+age = input("How old are you?")
+place = input("Where do you live?")
+print("Hello", name, "Your age is", age, "You live in", place)
