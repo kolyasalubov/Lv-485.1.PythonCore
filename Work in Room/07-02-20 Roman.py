4e2955323a1ede4ab918c7e7e533d4c426c8ee72
Roman <elgoog6651@gmail.com>
2020-02-08 01:10:28 +0200

    work in room

---
+++ Work in Room/07-02-20 Roman.py
@@ -0,0 +1,19 @@
+#a = int(input("ввдедіть перше число:"))
+#b = int(input("ввдедіть друге число:"))
+#if a >= b:
+#   print(a, b)
+#else:
+#   print(b, a)
+#
+#a1 = int(input("ввдедіть число:"))
+#if a1 % 2 == 0:
+#   print("парне")
+#print("не парне")
+# Factorial
+number = input('enter numbre :')
+factorial = 1
+for i in range(1, int(number) + 1):
+    factorial *= i
+print(factorial)
+#
+#print(list(range(2,100,2)))
